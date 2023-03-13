45b96e23ccf0eadcaefee5cb52ddd203ba893b7d
Romira915 <40430090+Romira915@users.noreply.github.com>
2023-03-13 15:16:06 +0900

    キャラの位置同期させる

--- src/app/components/myself.rs
+++ src/app/components/myself.rs
@@ -3,7 +3,7 @@ use std::{borrow::Borrow, cell::RefCell, rc::Rc};
 use wasm_bindgen::{prelude::Closure, JsCast};
 use web_sys::{HtmlElement, WebSocket};
 use yew::prelude::*;
-use yew_hooks::use_bool_toggle;
+use yew_hooks::{use_bool_toggle, use_interval};
 use yewdux::prelude::use_store_value;
 
 use crate::{
@@ -32,6 +32,35 @@ pub(crate) fn Myself(props: &MyselfProps) -> Html {
     let balloon_node_ref = use_node_ref();
     let is_active = use_bool_toggle(false);
     let chat_text_hash = use_store_value::<ChatTextHashState>();
+
+    // let send_my_pos = {
+    //     let username = username.clone();
+    //     let myself_rect = myself_rect.clone();
+    //     let ws = ws.clone();
+    //     move || {
+    //         if let Some(myself_rect) = (*myself_rect).clone() {
+    //             let my_pos = MyLocation {
+    //                 action: LocationType::UpdateCharacterPos,
+    //                 user_id: username.0.clone(),
+    //                 pos_x: myself_rect.left() + 32.,
+    //                 pos_y: myself_rect.top() + 32.,
+    //             };
+    //             if let Err(send_result) = (*ws)
+    //                 .borrow()
+    //                 .clone()
+    //                 .unwrap()
+    //                 .send_with_str(&serde_json::to_string(&my_pos).unwrap())
+    //             {
+    //                 log::error!(
+    //                     "Failed to Websocket send error. {}",
+    //                     send_result.as_string().unwrap_or_default()
+    //                 );
+    //             } else {
+    //                 log::debug!("Success websocket send");
+    //             }
+    //         }
+    //     }
+    // };
 
     {
         let username = username.clone();
@@ -98,31 +127,12 @@ pub(crate) fn Myself(props: &MyselfProps) -> Html {
                 let mouseup_listener = Closure::<dyn Fn(MouseEvent)>::wrap(Box::new({
                     let is_active = is_active.clone();
                     let myself_rect = myself_rect.clone();
-                    let ws = ws;
+                    let ws = ws.clone();
                     move |_e| {
                         log::debug!("on disactive");
                         is_active.set(false);
-                        if let Some(myself_rect) = (*myself_rect).clone() {
-                            let my_pos = MyLocation {
-                                action: LocationType::UpdateCharacterPos,
-                                user_id: username.0.clone(),
-                                pos_x: myself_rect.left(),
-                                pos_y: myself_rect.top(),
-                            };
-                            // let s = (*ws).send(&serde_json::to_string(&my_pos).unwrap());
-                            if let Err(send_result) = (*ws)
-                                .borrow()
-                                .clone()
-                                .unwrap()
-                                .send_with_str(&serde_json::to_string(&my_pos).unwrap())
-                            {
-                                log::error!(
-                                    "Failed to Websocket send error. {}",
-                                    send_result.as_string().unwrap_or_default()
-                                );
-                            } else {
-                                log::debug!("Success websocket send");
-                            }
+                        if let Some(myself_rect) = (*myself_rect).as_ref() {
+                            send_my_pos(username.0.as_str(), myself_rect, ws.clone());
                         }
                     }
                 }));
@@ -150,7 +160,20 @@ pub(crate) fn Myself(props: &MyselfProps) -> Html {
         );
     }
 
-    {}
+    use_interval(
+        {
+            let username = username.clone();
+            let myself_rect = myself_rect.clone();
+            let ws = ws.clone();
+
+            move || {
+                if let Some(myself_rect) = (*myself_rect).as_ref() {
+                    send_my_pos(username.0.as_str(), myself_rect, ws.clone());
+                }
+            }
+        },
+        5000,
+    );
 
     // Iconが押された時
     let onmousedown = {
@@ -187,5 +210,32 @@ pub(crate) fn Myself(props: &MyselfProps) -> Html {
             }
             </Balloon>
         </div>
+    }
+}
+
+fn send_my_pos(
+    username: &str,
+    myself_rect: &PageOffsetDomRect,
+    ws: Rc<RefCell<Option<WebSocket>>>,
+) {
+    let my_pos = MyLocation {
+        action: LocationType::UpdateCharacterPos,
+        user_id: username.to_string(),
+        // 32. 良くない
+        pos_x: myself_rect.left() + 32.,
+        pos_y: myself_rect.top() + 32.,
+    };
+    if let Err(send_result) = (*ws)
+        .borrow()
+        .clone()
+        .unwrap()
+        .send_with_str(&serde_json::to_string(&my_pos).unwrap())
+    {
+        log::error!(
+            "Failed to Websocket send error. {}",
+            send_result.as_string().unwrap_or_default()
+        );
+    } else {
+        log::debug!("Success websocket send");
     }
 }
